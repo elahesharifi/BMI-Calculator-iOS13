33f85e1acaba7f2b4298414ad6431eee731b7a49
elahe <el.sh192014@gmail.com>
2022-05-24 20:57:30 +0200

    add secondViewController Class

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Elahe  Sharifi on 24/05/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+
+import UIKit
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
