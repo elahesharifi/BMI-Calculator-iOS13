8fc8be5d66d5fbf12f450aaa2d7e0a801cd5d499
elahe <el.sh192014@gmail.com>
2022-05-26 19:29:35 +0200

    calculateBMI func & pass data to ResultViewController

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Elahe  Sharifi on 26/05/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    var value: Float
+    let advice : String
+    let color: UIColor
+}
--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,7 +6,33 @@
 //  Copyright © 2022 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
 struct CalculatorBrain {
+    
+    var bmi : BMI?
+    
+    func getBMIValue() -> String {
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
+        return bmiTo1DecimalPlace
+    }
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No Advice"
+    }
+    func getColor() -> UIColor {
+        return bmi?.color ?? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    mutating func calulateBMI(height:Float ,weight: Float) {
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more Pies", color: .blue)
+        }else if bmiValue < 24.5{
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle", color: .green)
+        }else {
+            bmi = BMI(value: bmiValue, advice: "Eat less Pies", color: .blue)
+        }
+    }
     
 }
